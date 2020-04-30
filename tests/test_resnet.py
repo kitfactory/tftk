8989ae76c10ac93e6fd834e568235cd82d020e03
kitfactory <kitfactory@gmail.com>
2020-04-30 14:14:28 +0900

    update test

--- tests/test_resnet.py
+++ tests/test_resnet.py
@@ -4,6 +4,12 @@ from tftk.image.model import AbstractClassificationModel
 
 from tftk.image.model.small_resnet_keras_contrib import ResNet18
 
+
+## default is channel last
+ROW_AXIS = 1
+COL_AXIS = 2
+CHANNEL_AXIS = 3
+
 def shortcut(input:tf.keras.layers.Layer,output:tf.keras.layers.Layer)->tf.keras.layers.Layer:
     if input.shape != output.shape: # ショートカットにダウンサンプルが必要
         x = tf.keras.layers.AveragePooling2D(input) # ResNet-D
@@ -11,22 +17,32 @@ def shortcut(input:tf.keras.layers.Layer,output:tf.keras.layers.Layer)->tf.keras
     return x
 
 def get_layer_name(layer:int, block:int, elem:str):
-    return "{}_{}_{}".format(elem,layer,block)
+    name = "{}_{}_{}".format(elem,layer,block)
+    print(name)
+    return name
 
 def get_residual_unit_v2(x:tf.keras.layers.Layer, layer:int, block:int, filters:int, kernel_size:(int,int)):
     # ResNet V2は、http://arxiv.org/pdf/1603.05027v2.pdf
     dilation_rate = (1,1)
     ki = "he_normal"
     kr = tf.keras.regularizers.l2(1e-4)
+    
+    print("get residual unit layer",layer,"block",block, "Tensor",x)
 
     if block!=1:
         # ダウンサンプルがいらない
         strides = (1,1)
-        x = tf.keras.layers.BatchNormalization(name=get_layer_name(layer=layer,block=block,elem="batchnorm"))(x)
+        x = tf.keras.layers.BatchNormalization(axis=CHANNEL_AXIS,name=get_layer_name(layer=layer,block=block,elem="batchnorm"))(x)
         x = tf.keras.layers.Activation("relu", name=get_layer_name(layer=layer,block=block,elem="relu"))(x)
-        x = tf.keras.layers.("relu", name=get_layer_name(layer=layer,block=block,elem="relu"))(x)
+        x = tf.keras.layers.Conv2D(filters=filters, kernel_size=kernel_size,
+                      strides=strides, padding="same",
+                      dilation_rate=dilation_rate,
+                      kernel_initializer=ki,
+                      kernel_regularizer=kr,
+                      name=get_layer_name(layer=layer,block=block,elem="conv2d"))(x)
     elif layer == 2 and block==1:
-        # ダウンサンプル、BN/Reluが要らない
+        # MaxPoolingでダウンサンプル、BN/Reluが要らない
+        x = tf.keras.layers.MaxPooling2D(pool_size=(3, 3), strides=(2,2), padding="same")(x)
         strides = (1,1)
         x = tf.keras.layers.Conv2D(filters=filters, kernel_size=kernel_size,
                       strides=strides, padding="same",
@@ -37,7 +53,7 @@ def get_residual_unit_v2(x:tf.keras.layers.Layer, layer:int, block:int, filters:
     else:
         # BN/Reluが要る、ダウンサンプルする。
         strides = (2,2)
-        x = tf.keras.layers.BatchNormalization(name=get_layer_name(layer=layer,block=block,elem="batchnorm"))(x)
+        x = tf.keras.layers.BatchNormalization(axis=CHANNEL_AXIS,name=get_layer_name(layer=layer,block=block,elem="batchnorm"))(x)
         x = tf.keras.layers.Activation("relu", name=get_layer_name(layer=layer,block=block,elem="relu"))(x)
         x = tf.keras.layers.Conv2D(filters=filters, kernel_size=kernel_size,
                       strides=strides, padding="same",
@@ -45,20 +61,18 @@ def get_residual_unit_v2(x:tf.keras.layers.Layer, layer:int, block:int, filters:
                       kernel_initializer=ki,
                       kernel_regularizer=kr,
                       name=get_layer_name(layer=layer,block=block,elem="conv2d"))(x)
+    return x
 
-def basic_block(input:tf.keras.layers.Layer,  layer:int, filters:int, repetitions:int , downsample:bool):
+def basic_block(input:tf.keras.layers.Layer, layer:int, filters:int):
 
-    if layer == 2:
-        # ダウンサンプルはmax pooling
-        x = tf.keras.layers.MaxPooling2D(pool_size=(3, 3), strides=(2,2), padding="same", name=get_layer_name(layer,1,"maxpooling"))(x)
+    print("basic block layer",layer)
+    x = get_residual_unit_v2(x=input,layer=layer,block=1, filters=filters, kernel_size=(3,3))
 
-    x = get_residual_unit_v2(x=x, layer=2,block=1, filters=filters)
-
-    x = get_residual_unit_v2(x=x, layer=2,block=2, filters=filters)
+    x = get_residual_unit_v2(x=x,layer=layer,block=2, filters=filters, kernel_size=(3,3))
     
     # x = tf.keras.
-    s = shortcut(input,x)
-    x = tf.keras.layers.Add([x,s]) #  shortcut path
+    # s = shortcut(input,x)
+    # x = tf.keras.layers.Add([x,s]) #  shortcut path
     return x
 
 def get_resnet(input_shape)->tf.keras.Model:
@@ -91,14 +105,15 @@ def get_resnet(input_shape)->tf.keras.Model:
 
     # Conv1
     img_input = tf.keras.Input(shape=input_shape, name='input')
-    x = tf.keras.layers.Conv2D(filters=initial_filters,kernel_size=initial_kernel_size,strides=initial_strides, kernel_initializer=ki, kernel_regularizer=kr,padding="same")(img_input)
-    x = tf.keras.layers.BatchNormalization(name='conv1_batchnorm')(x)
-    x = tf.keras.layers.Activation("relu", name='conv1_relu')(x)
+    x = tf.keras.layers.Conv2D(filters=initial_filters,kernel_size=initial_kernel_size,strides=initial_strides, kernel_initializer=ki, kernel_regularizer=kr,padding="same",name=get_layer_name(1,0,"conv2d"))(img_input)
+    x = tf.keras.layers.BatchNormalization(name=get_layer_name(1,0,"barchnorm"))(x)
+    x = tf.keras.layers.Activation("relu", name=get_layer_name(1,0,"relu"))(x)
 
     filters = initial_filters
-    for r in range(repeatations):
-         for idx in range(r):
-             x = basic_block(x, layer=(r + 2), filter=filters)
+    for idx,r in enumerate(repeatations):
+        print("for loop  r", r, "idx", idx)
+        x = basic_block(x, layer=(idx + 2), filters=filters)
+        filters = filters*2
     
     model = tf.keras.Model(img_input, x)
     model.summary()
